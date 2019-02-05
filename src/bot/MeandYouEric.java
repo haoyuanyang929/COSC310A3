46d3f9a8a04182b91bbbc3d4a1bdc4bebe3db41b
haoyuanyang929 <43188567+haoyuanyang929@users.noreply.github.com>
2019-02-04 16:50:06 -0800

    Created a class call MeandYouEric (Q+A).Implemented topicquestion class.All the responds are in the string array form.

---
+++ src/bot/MeandYouEric.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class MeandYouEric {
+	
+	String[] who = {"I am a dinosaur.", "I am Tee-Tee.", "I am a T-rex. You can call me Tee-Tee.", "I am a ancient giant chicken.", "You are my lunch.", "You are a human and also my lunch."}; 
+	String[] what = {"I am a dinosaur.", "I am Tee-Tee.", "I am a T-rex. You can call me Tee-Tee.", "I am a ancient giant chicken.", "I can eat, eat, and… Talk to you.", "I like food and other things."};
+	String[] when = {"During luchtime", "Durig dinner time.", "Perhaps during breakfest time.", "Two days ago.", "It’s Triassic Period.","It's "/*today date*/, "It's " /*today's day*/, "250 million years later."};
+	String[] how = {"I am starving", "I am good.", "How rude! I am 35 years old", "My age is a secret.", "I am 40 ft tall.", "I am 12.3 meters tall.", "He died from a fireball from the sky."};
+	String[] why = {"I have no idea.", "Because I am hungry.","Because you look delicious and juicy.","Because I am adorable.","Because I am a hardworking, and talented T-rex.","Because you are here." ,"Because… Because… Because…. What did you ask me again?",	"Because I am a dinosaur."};
+}
