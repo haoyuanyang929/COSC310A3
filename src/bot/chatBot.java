3c1607e68cbcb9a22a8dd414e2846ec42f10aa2a
gdbt <gdbt@hotmail.ca>
2019-02-08 13:18:38 -0800

    Noah Big update 7
    chatBot.java
    	- Changed inputs to convert to lowercase (excluding name)
    	- Updated greeting to have bot's name
    	- Added the quit option to terminate program
    	- Blocked out the thinking method due to an error in the timing.
    	- Added bridge to go to collection.java
    collection.java
    	-completed the collection hobby topic zone.
    	-added options for all birthstone gems
    	-error checked to make sure all messages would print
    	-added bridge to go back to main chatBot.java

--- src/bot/chatBot.java
+++ src/bot/chatBot.java
@@ -9,11 +9,10 @@ public class chatBot {
 		//Loadingscrean.main(args); 
 		
 		System.out.println("ROAr roar roooar roar rrrroooaaaarrr RoaaaaaaaaaaaaaaaAAArrArArAr");
-		System.out.println("(Welcome to the Dino-Bot, Please Enter the name you would like to be called by.)");
+		System.out.println("Welcome to the Dino-Bot, Please Enter the name you would like to be called by.");
 		Scanner mainscan = new Scanner(System.in);
 		name = mainscan.nextLine();
-		System.out.println("Rouarar " + name);
-		System.out.println("(Greetings " + name + ")");
+		System.out.println("Greetings " + name + ". My name is Tee-Tee the T-Rex!!");
 		
 		//feel free to change later, just did this so i could access code from main area
 		inputfield();
@@ -25,10 +24,12 @@ public class chatBot {
 	public static void inputfield(){ //input method. Subject to change like crazy
 		Scanner inscan = new Scanner(System.in);
 		String startoption;
+		System.out.println("..........."); //spacer
 		System.out.println("Curious to know what I can talk about? Have a list"); //prints list of topics
-		System.out.println(thinking());
-		System.out.println(thinking());
-		System.out.println(thinking());
+//		System.out.println(thinking());	  ************************************************
+//		System.out.println(thinking());   *ISSUE WITH THINKING METHOD OMITTED FOR TESTING*
+//		System.out.println(thinking());   ************************************************
+		System.out.println("..........."); //spacer
 		System.out.println("We can test how well your roar is!");
 		System.out.println("Or we can talk about food");
 		System.out.println("I know a decent amount about INSERT TOPIC 1");
@@ -37,13 +38,13 @@ public class chatBot {
 		System.out.println("INSERT TOPIC 4 happens to be something that interests me");
 		System.out.println("Or maybe you want to talk about INSERT TOPIC 5");
 		System.out.println("Or if you want to get to know me thats fine too!");
-		System.out.println(thinking());
+		//System.out.println(thinking());
 		
 		
 		//checking what they are talking about.
 		
 		
-		startoption = inscan.nextLine(); //scanning an input
+		startoption = (inscan.nextLine()).toLowerCase(); //scanning an input
 		//roar else
 		if(startoption.contains("roar")) { //if it contains roar jump to roar file
 			boolean grab = checktopic("roar"); //checking topic for user confirmation
@@ -60,11 +61,9 @@ public class chatBot {
 		else if(startoption.contains("collection")) {
 			boolean grab = checktopic("Collections"); //checking topic for user confirmation
 			if(grab == true) {
-				System.out.println("collection accepted");
 				Collectiontopic.main(null); //if user says yes it goes to collection topic
 			}
 			else {
-				System.out.println("collection denied");
 				inputfield(); //if user say no it goes back to input
 			}
 		}
@@ -80,20 +79,26 @@ public class chatBot {
 				inputfield();
 			}
 		}
+		else if(startoption.contains("quit") || startoption.contains("leave") || startoption.contains("stop")) {
+			System.out.println("Thank you so much for talking to me! Have a good day!");
+			System.exit(0);
+		}
+		//if it doesn't understand
+		else {
 		System.out.println("Sorry I don't understand that."); //nothing useful in the phrase
 		inputfield(); //go back to input and try again
-			
+		}
 	}
 	
-	public static String thinking(){ //prints a line of dots after 1.5 seconds to mimic thinking animation
-		long curtime = System.currentTimeMillis();
-		long waittime1 = System.currentTimeMillis() + 150;
-		while (curtime != waittime1){
-			 curtime = System.currentTimeMillis();
-		}
-		 return "............";
-
-	}
+//	public static String thinking(){ //prints a line of dots after 1.5 seconds to mimic thinking animation
+//		long curtime = System.currentTimeMillis();
+//		long waittime1 = curtime + 150;
+//		while (curtime != waittime1){				************************************************
+//			 curtime = System.currentTimeMillis();  *ISSUE WITH THINKING METHOD OMITTED FOR TESTING*
+//		}                                           ************************************************
+//		 return "............";
+//
+//	}
 	//CHECKING THE TOPIC
 	public static boolean checktopic(String top) { //grabs the topic, to save space this is used fo most topics
 		Scanner topche = new Scanner(System.in);
@@ -103,7 +108,7 @@ public class chatBot {
 		else {
 			System.out.println("Sorry! My dino-speech is better than my english, did you want to talk about " + top + " ?"); //maybe you didn't mean to go here
 		}
-		String topicyesno = topche.nextLine();
+		String topicyesno = (topche.nextLine()).toLowerCase();
 		String[] topicsentence = topicyesno.split(" "); //splitting it up
 		for(int i = 0; i < topicsentence.length; i++){ //
 			String topgrabbed = topicsentence[i];
