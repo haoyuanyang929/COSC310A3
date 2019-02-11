072638df69440964a7d3b77defc693f6ca19b7f0
haoyuanyang929 <43188567+haoyuanyang929@users.noreply.github.com>
2019-02-11 13:30:25 -0800

    Added some responds to food

--- src/bot/food.java
+++ src/bot/food.java
@@ -2,10 +2,10 @@ package bot;
 
 public class food {
 	public static void main(String args[]) {
-		String[] what = {"Meat, like human", "Human, just like you", "Bubble Tea"};
-		String[] where = {"In my cave", ""};
+		String[] what = {"Meat, like human", "Human, just like you", "Bubble Tea", "It's a delisious drink."};
+		String[] where = {"In my cave", "In Taiwan"};
 		String[] Do = {"Yes, I like"};
-		String[] who = {"Sometimes with myself", "Sometimes with other dinosaurs", "by myself"};
-		String[] how = {"four times", "It's so delicious", "By my hand"};
+		String[] who = {"Sometimes with myself", "Sometimes with other dinosaurs", "by myself", "The owner."};
+		String[] how = {"four times", "It's so delicious", "By my hand", "It's a combination of tea and milk and tapioca balls.", "I like it half sweet."};
 	}
 }
