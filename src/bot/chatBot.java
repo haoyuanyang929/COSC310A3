068c099ebb7f7e0fdfbe32b199aa14d62cb40b3c
gdbt <gdbt@hotmail.ca>
2019-02-13 15:44:18 -0800

    Noah Update 12
    	-Fixed the ERROR 63 Line.
    	-Added More phrases

--- src/bot/chatBot.java
+++ src/bot/chatBot.java
@@ -85,7 +85,7 @@ public class chatBot {
 	//CHECKING THE TOPIC
 	public static void checktopic(String top) { //grabs the topic, to save space this is used fo most topics
 		Scanner topche = new Scanner(System.in);
-		if(top.equals("roar")) {
+		if(top.contains("roar")) {
 			System.out.println("Sorry! My dino-speech is better than my english, did you want to have a roaring contest?"); //maybe you didn't mean to go here
 			String topicyesno = (topche.nextLine()).toLowerCase();
 			String[] topicsentence = topicyesno.split(" "); //splitting it up
