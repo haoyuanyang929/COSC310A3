d8acb901c3e6d51ee513a897b327f5eff435fd3f
haoyuanyang929 <43188567+haoyuanyang929@users.noreply.github.com>
2019-02-13 13:38:06 -0800

    me method all completed for all the respond saved in me

--- src/bot/me.java
+++ src/bot/me.java
@@ -177,13 +177,77 @@ public class me implements TopicQuestions {
 	@Override
 	public void whyQuestion(String input) {
 		// TODO Auto-generated method stub
-
+//		 why = {0 "I have no idea.", 
+//				1 "Because I am hungry.", 
+//				2 "Because you look delicious and juicy.",
+//				3"Because I am adorable.", 
+//				4"Because I am a hardworking, and talented T-rex.", 
+//				5"Because you are here.",
+//				6"Because… Because… Because…. What did you ask me again?", 
+//				7"Because I am a dinosaur." };
+		if (input.contains("eat") || input.contains("food")) {
+			int i;
+			double a = Math.random() * 100 / 4;
+			if (a <= 25)
+				i = 1;
+			else if (a > 25 && a <= 50)
+				i = 2;
+			else if (a > 50 && a <= 75)
+				i = 5;
+			else
+				i = 7;
+			System.out.println(why[i]);// 1,2,5,7
+			
+		}else if(input.contains("good") || input.contains("excellent")) {
+			int i;
+			double a = Math.random() * 100 / 2;
+			if (a <= 50) i = 3;
+			else i = 4;
+			System.out.println(why[i]);// 3,4
+		}else {
+			int i;
+			double a = Math.random() * 100 / 2;
+			if (a <= 50) i = 0;
+			else i = 7;
+			System.out.println(why[i]);// 0,7
+		}
+		
 	}
 
 	@Override
 	public void howQuestion(String input) {
 		// TODO Auto-generated method stub
-
+//		how = { 0"I am starving", 
+//				1"I am good.", 
+//				2"How rude! I am 35 years old", 
+//				3"My age is a secret.",
+//				4"I am 40 ft tall.", 
+//				5"I am 12.3 meters tall.", 
+//				6"He died from a fireball from the sky." };
+		if (input.contains("you") && (input.contains("are") || input.contains("do"))) {
+			int i;
+			double a = Math.random() * 100 / 2;
+			if (a <= 50) i = 0;
+			else i = 1;
+			System.out.println(how[i]);// 0,1
+		} else if ( input.contains("age") || input.contains("old")) {
+			int i;
+			double a = Math.random() * 100 / 2;
+			if (a <= 50) i = 2;
+			else i = 3;
+			System.out.println(how[i]);// 2,3
+		} else if ( input.contains("tall") || input.contains("long") || input.contains("big") || input.contains("size")) {
+			int i;
+			double a = Math.random() * 100 / 2;
+			if (a <= 50) i = 4;
+			else i = 5;
+			System.out.println(how[i]);// 4,5
+		} else if ( input.contains("die") || input.contains("dead") || input.contains("died")) {
+			System.out.println(how[6]);
+		}else {
+			System.out.println("My knowledge is limited. Can you reword it?");
+		}
+		 
 	}
 
 }
