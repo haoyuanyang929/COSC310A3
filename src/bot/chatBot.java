bcd74353e0a5a2938d3aaff7512506325e9167bc
Kathryn Ng <ng.kathryn@gmail.com>
2019-02-15 11:16:39 -0800

    updated help and timing/thinking

--- src/bot/chatBot.java
+++ src/bot/chatBot.java
@@ -11,14 +11,14 @@ public class chatBot {
 		//added a cute little printing thing. lemme know thoughts. keep it until we get GUI
 
 		irrTopic = new IrrelavantTopic();
-	//	Loadingscrean.main(args); 
+		Loadingscrean.main(args); 
 
 		System.out.println("ROAr roar roooar roar rrrroooaaaarrr RoaaaaaaaaaaaaaaaAAArrArArAr");
 		System.out.println("Welcome to the Dino-Bot, Please Enter the name you would like to be called by.");
 		Scanner mainscan = new Scanner(System.in);
 		name = mainscan.nextLine();
 		System.out.println("Greetings " + name + ". My name is Tee-Tee the T-Rex!!");
-		
+		System.out.println("If you are curious type \"help\" to get a list of topics I will talk to you about!");
 		//feel free to change later, just did this so i could access code from main area
 		inputfield();
 		mainscan.close();
@@ -29,23 +29,6 @@ public class chatBot {
 	public static void inputfield(){ //input method. Subject to change like crazy
 		Scanner inscan = new Scanner(System.in);
 		String startoption;
-		
-		System.out.println("..........."); //spacer
-//		System.out.println("Curious to know what I can talk about? Have a list"); //prints list of topics
-//		System.out.println(thinking());	  //************************************************
-//		System.out.println(thinking());   //*ISSUE WITH THINKING METHOD OMITTED FOR TESTING*
-//		System.out.println(thinking());   //************************************************
-		System.out.println("..........."); //spacer
-		System.out.println("We can test how well your roar is!");
-		System.out.println("Or we can talk about food");
-		System.out.println("I know a decent amount about INSERT TOPIC 1");
-		System.out.println("We can discuss collection!");
-		System.out.println("How about talking about INSERT TOPIC 3");
-		System.out.println("INSERT TOPIC 4 happens to be something that interests me");
-		System.out.println("Or maybe you want to talk about INSERT TOPIC 5");
-		System.out.println("Or if you want to get to know me thats fine too!");
-	//	System.out.println(thinking());
-		
 		
 		//checking what they are talking about.
 		boolean continueConv;
@@ -77,7 +60,7 @@ public class chatBot {
 	public static String thinking(){ //prints a line of dots after 1.5 seconds to mimic thinking animation
 		long curtime = System.currentTimeMillis();
 		
-		long waittime1 = curtime + 150;
+		long waittime1 = curtime + 1000;
 		while (curtime != waittime1){				
 			 curtime = System.currentTimeMillis();  
 		}                                           
@@ -109,6 +92,15 @@ public class chatBot {
 				inputfield();
 			}
 		}
+		else if(top.equals("help")) {
+			helpMe();
+			inputfield();
+		}
+		else if(top.equals("about")) {
+			aboutMe();
+			inputfield();
+		}
+		//swear filter
 		else if(top.contains("fuck") || top.contains("bitch") || top.contains("shit") || top.contains("ass") || top.contains("fucking") || top.contains("motherfucker") || top.contains("damn")){
 			System.out.println("Please do not swear at me!");
 			thinking();
@@ -119,6 +111,28 @@ public class chatBot {
 			thinking();
 			System.exit(0); //closing the program forcibly
 		}
+	}
+	
+	//helpMe prompts user with list of topics the bot is able to talk about.
+	public static void helpMe() {
+		System.out.println("Curious to know what I can talk about? Have a list"); //prints list of topics
+		System.out.println(thinking());	  //************************************************
+		System.out.println(thinking());   //*ISSUE WITH THINKING METHOD OMITTED FOR TESTING*
+		System.out.println(thinking());   //************************************************
+		System.out.println("..........."); //spacer
+		System.out.println("We can test how well your roar is!");			 	//Prompts roar contest
+		System.out.println("Or we can talk about food");						//Prompts food topic
+		System.out.println("We can discuss my rock collection!");				//Prompts collection topic
+		System.out.println("How about talking about Lord of The Rings?");		//Prompts Lord of The Rings topic
+		System.out.println("Or if you want to get to know me thats fine too!");	//Prompts general qna
+		System.out.println(thinking());
+		
+	}
+	
+	public static void aboutMe() {
+		System.out.println("My name is Tee-Tee and I am a Java jar program,");
+		System.out.println("coded by Eric, Noah, Yue, Aidan and Kathryn - for their COSC 310 Assignment.");
+		System.out.println("They hope you'll get the full experience of talking to an old dinosaur like me!");
 	}
 	
 }
